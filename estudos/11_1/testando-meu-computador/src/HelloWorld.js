3cba0a2a4b0d6359e116242c9b10944f7dda26cd
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-09 19:16:31 -0300

    Fazendo os exercícios React 11.1

---
+++ estudos/11_1/testando-meu-computador/src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <span>Oi Mundo!</span>
+    )
+  }
+}
+
+export default HelloWorld;
